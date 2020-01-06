6299cbc2d9fdc5d18154a3ffdc70c07f24fb2b3b
Daniel Koch <daniel.koch@helsinki.fi>
2020-01-06 20:29:18 +0200

    ci

---
+++ ci/test.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -x
+
+echo "Testing for $TEST_PLATFORM"
+
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
+  -projectPath $(pwd) \
+  -logFile /dev/stdout \
+  -batchmode \
+  -manualLicenseFile /root/.local/share/unity3d/Unity/Unity_lic.ulf
+
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
+  -projectPath $(pwd) \
+  -runTests \
+  -testPlatform $TEST_PLATFORM \
+  -testResults $(pwd)/$TEST_PLATFORM-results.xml \
+  -logFile /dev/stdout \
+  -batchmode
+
+UNITY_EXIT_CODE=$?
+
+if [ $UNITY_EXIT_CODE -eq 0 ]; then
+  echo "Run succeeded, no failures occurred";
+elif [ $UNITY_EXIT_CODE -eq 2 ]; then
+  echo "Run succeeded, some tests failed";
+elif [ $UNITY_EXIT_CODE -eq 3 ]; then
+  echo "Run failure (other failure)";
+else
+  echo "Unexpected exit code $UNITY_EXIT_CODE";
+fi
+
+cat $(pwd)/$TEST_PLATFORM-results.xml | grep test-run | grep Passed
+exit $UNITY_EXIT_CODE
