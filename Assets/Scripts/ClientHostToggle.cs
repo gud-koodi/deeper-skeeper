c810868df238b5adf647828e1f7d8026307a15f1
jolampi <36279434+jolampi@users.noreply.github.com>
2020-01-09 16:42:01 +0200

    Conditionally start as server, not synced yet

---
+++ Assets/Scripts/ClientHostToggle.cs
@@ -0,0 +1,26 @@
+﻿using DarkRift.Client.Unity;
+using DarkRift.Server.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClientHostToggle : MonoBehaviour {
+    public bool isHost = true;
+
+    // Start is called before the first frame update
+    void Start() {
+        if (isHost) {
+            print("Host Server");
+            SceneManager.LoadScene("ServerScene", LoadSceneMode.Single);
+        } else {
+            print("Load Scene");
+            SceneManager.LoadScene("DragDemo", LoadSceneMode.Single);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Server : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        print("Load scene");
+        SceneManager.LoadScene("DragDemo", LoadSceneMode.Additive);
+    }
+}
