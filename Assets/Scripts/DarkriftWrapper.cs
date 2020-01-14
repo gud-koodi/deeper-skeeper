9e2b907b8d67fd52d3f4fcca297cdfbe849d500b
Daniel Koch <daniel.koch@helsinki.fi>
2020-01-14 17:28:22 +0200

    tests working

---
+++ Assets/Scripts/DarkriftWrapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DarkRift;
+
+public class DarkriftWrapper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public DarkRiftWriter MyDarkriftWriter { get; set; }
+
+    public void initDarkriftWriter()
+    {
+    }
+    void Start()
+    {
+        Debug.Log("AWAKE");
+        using (DarkRiftWriter writer = DarkRiftWriter.Create()) {}
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
