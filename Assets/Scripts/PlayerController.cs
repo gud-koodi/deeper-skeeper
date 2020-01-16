e3e5de976ce38fedf2aaa38ee545888c97642aaa
Daniel Koch <daniel.koch@helsinki.fi>
2020-01-16 17:47:39 +0200

    movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    // Start is called before the first frame update
+    public Rigidbody playerRigidbody;
+    public float speed = 10.0f;
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        float mH = Input.GetAxis ("Horizontal");
+        float mV = Input.GetAxis ("Vertical");
+        playerRigidbody.velocity = new Vector3 (mH * speed, playerRigidbody.velocity.y, mV * speed);
+
+    }
+}
