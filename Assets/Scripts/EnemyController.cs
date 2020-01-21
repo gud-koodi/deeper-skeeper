5c2409fa1fce5f0dfbb5ef838f9ba6d286d3ac0d
Daniel Koch <daniel.ukonhattu@gmail.com>
2020-01-22 00:03:27 +0200

    CylinderMan

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+
+{
+    private UnityEngine.AI.NavMeshAgent agent;
+    public GameObject player;
+    enum State
+    {
+        IDLE,
+        CHASE
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = player.transform.position;
+    }
+}
--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Sword : Weapon {
     public float damage;
     public float sweepReach;
