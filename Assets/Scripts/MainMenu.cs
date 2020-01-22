4f5ec54d7739999dc5a620be5e1abb0663e9dc0a
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2020-01-22 15:41:48 +0200

    Added network configuration and simple main menu assets

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public NetworkConfig networkConfig;
+
+    /// <summary>
+    /// Start the game without host configuration.
+    /// </summary>
+    public void Connect() {
+        networkConfig.isHost = false;
+        LoadScene();
+    }
+
+    /// <summary>
+    /// Start the game with host configuration.
+    /// </summary>
+    public void Host() {
+        networkConfig.isHost = true;
+        LoadScene();
+    }
+
+    /// <summary>
+    /// Quits the game.
+    /// </summary>
+    public void Quit() {
+        Application.Quit();
+    }
+
+    private void LoadScene() {
+        Debug.Log("Starting the game as " + ((networkConfig.isHost) ? "host" : "client"));
+        SceneManager.LoadScene("ClickerTest", LoadSceneMode.Single);
+    }
+
+}
