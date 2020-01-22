5d5bbdc9e4738f40131b4e726d9028910b2fb0d3
Daniel Koch <daniel.ukonhattu@gmail.com>
2020-01-22 19:00:11 +0200

    ai

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,20 +7,68 @@ public class EnemyController : MonoBehaviour
 {
     private UnityEngine.AI.NavMeshAgent agent;
     public GameObject player;
-    enum State
+    public State state;
+    public enum State
     {
         IDLE,
-        CHASE
+        CHASE,
+        ATTACK
     }
+    private Animator animator;
+    public Weapon weapon;
+    public float hitSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
         agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        state = State.CHASE;
+        animator = gameObject.GetComponent<Animator>();
+        animator.SetFloat("hitSpeed", hitSpeed);
+        weapon.AttackDuration = weapon.AttackDuration / hitSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (state)
+        {
+            case State.CHASE:
+                Chase();
+                break;
+            case State.IDLE:
+                break;
+            case State.ATTACK:
+                Attack();
+                break;
+        }
+    }
+
+    private void Chase()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < 5)
+        {
+            
+            agent.isStopped = true;
+            state = State.ATTACK;
+            return;
+        }
+        agent.isStopped = false;
         agent.destination = player.transform.position;
+        animator.SetBool("isWalking", true);
+    }
+
+    private void Attack()
+    {
+        weapon.Attack();
+        animator.SetBool("isAttacking", true);
+        state = State.IDLE;
+        StartCoroutine(WaitAttack());
+    }
+
+    private IEnumerator WaitAttack() {
+        yield return null;
+        animator.SetBool("isAttacking", false);
+        yield return new WaitForSeconds(weapon.AttackDuration);
+        state = State.CHASE;
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,5 +1,12 @@
 using UnityEngine;
-public abstract class Weapon : MonoBehaviour {
+public abstract class Weapon : MonoBehaviour
+{
+    public float AttackDuration { get; set; }
+    public float attackDuration;
+    public float damage;
     public abstract void Attack();
-
+    public virtual void Start()
+    {
+        AttackDuration = attackDuration;
+    }
 }
