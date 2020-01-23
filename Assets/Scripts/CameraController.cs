356e9b5ac65787892df091c64f7b43d9af9f1c1e
Daniel Koch <daniel.koch@helsinki.fi>
2020-01-23 17:43:08 +0200

    fix format

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,19 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
+public class CameraController : MonoBehaviour {
     public GameObject player;
     private Vector3 offset;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
-    void LateUpdate()
-    {
+    void LateUpdate() {
         transform.position = player.transform.position + offset;
     }
 }
