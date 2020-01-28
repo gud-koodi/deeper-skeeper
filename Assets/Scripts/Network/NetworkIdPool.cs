3eb28e170fec4efc77aaba4f148494a2be48ca3f
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2020-01-29 01:05:41 +0200

    Fixed and test covered NetworkIdPool

--- Assets/Scripts/Network/NetworkIdPool.cs
+++ Assets/Scripts/Network/NetworkIdPool.cs
@@ -1,27 +1,50 @@
 using System;
 using System.Collections.Generic;
 
-namespace Network {
-    public class NetworkIdPool {
+namespace Network
+{
+    /// <summary>
+    /// Class for handling out sequential ids for objects and help with cache locality by recycling old ids.
+    /// </summary>
+    public class NetworkIdPool
+    {
         private ushort counter;
         private Queue<ushort> recyclables;
 
-        public NetworkIdPool() {
+        /// <summary>
+        /// Creates new instance of NetworkIdPool, that will start to hand out ids starting from zero.
+        /// </summary>
+        public NetworkIdPool()
+        {
             this.counter = 0;
             this.recyclables = new Queue<ushort>();
         }
 
-        public ushort Next() {
+        /// <summary>
+        /// Returns next, possibly recycled network id.
+        /// </summary>
+        /// <returns>The next usable id in this pool</returns>
+        public ushort Next()
+        {
             return (recyclables.Count > 0) ? recyclables.Dequeue() : counter++;
         }
 
-        public void Release(ushort id) {
-            if (id < counter) {
+        /// <summary>
+        /// Releases given id back to the pool of usable ids.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool Release(ushort id)
+        {
+            if (id < counter && !recyclables.Contains(id)) // TODO: implement a structure with faster search
+            {
                 recyclables.Enqueue(id);
-            } else {
-                throw new IndexOutOfRangeException();
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
-
 }
