1801fee17a8e54bf7a90603385689041c2920891
Daniel Koch <daniel.koch@helsinki.fi>
2020-02-03 12:56:20 +0200

    use System.Random instrad of Unity's Random

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,31 +4,35 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
-    public int LevelCounter { get; set; }
-    public int randomSeed;
 
     public Transform spawnPosition;
 
     private GameObject currentLevel;
     private GameObject nextLevel;
     private int depth = 10;
+    private System.Random random;
     private const int YDISTANCE = 50;
-    private const int TILESIZE = 30*2;
+    private const int TILESIZE = 30 * 2;
     private const int NEXT_TILE_OFFSET = TILESIZE;
-    // Start is called before the first frame update
+
+    public int RandomSeed { get; set; }
     void Start()
     {
-        LevelCounter = 1;
+        random = new System.Random(RandomSeed);
+        GenerateNext();
+    }
+
+    public void GenerateNext()
+    {
         float nextSpawnZ = spawnPosition.position.z;
         float nextSpawnX = spawnPosition.position.x;
         FloorInfo floorInfo;
-        Random.InitState(randomSeed + 1);
         for (int i = 0; i < depth; i++)
         {
             GameObject prefab = (GameObject)Resources.Load("Prefabs/Floor1", typeof(GameObject));
             nextLevel = Instantiate(prefab, new Vector3(nextSpawnX, -YDISTANCE * (i + 1), nextSpawnZ), prefab.transform.rotation);
             floorInfo = nextLevel.GetComponent<FloorInfo>();
-            int direction = Random.Range(1, 4);
+            int direction = random.Next(1, 4);
             if (direction == 1)
             {
                 nextSpawnZ = nextSpawnZ + NEXT_TILE_OFFSET;
