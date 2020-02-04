2ef22014133bbf11f0f9bfa1c147e1da07052efc
Daniel Koch <daniel.ukonhattu@gmail.com>
2020-02-04 21:29:22 +0200

    fix lint issues

--- Assets/Scripts/ICallable.cs
+++ Assets/Scripts/ICallable.cs
@@ -1,12 +1,13 @@
 /// <summary>
 /// Interface for a generic call wrapper.
 /// </summary>
+/// /// <typeparam name="T">Generic type.</typeparam>
 public interface ICallable<T>
 {
     /// <summary>
     /// Calls object.
     /// </summary>
-    /// <typeparam name="T">Generic type.</typeparam>
+    /// <typeparam name="t">Generic type.</typeparam>
     /// <param name="t">What to call.</param>
     void Call(T t);
 }
