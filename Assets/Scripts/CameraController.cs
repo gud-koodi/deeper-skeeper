6db5d784a72938ec46fd01dddb1b3660c913d354
Daniel Koch <daniel.koch@helsinki.fi>
2020-02-18 12:38:54 +0200

    hide walls if they are in between player and camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -18,9 +18,21 @@ public class CameraController : MonoBehaviour
     private UpdateStrategy updateStrategy = () => { };
 
     /// <summary>
+    /// Strategy for casting from camera.
+    /// </summary>
+    private CastStrategy castStrategy = () => { };
+
+    private List<MeshRenderer> disabledRenderers = new List<MeshRenderer>();
+
+    /// <summary>
     /// Strategy for updating the camera position.
     /// </summary>
     private delegate void UpdateStrategy();
+
+    /// <summary>
+    /// Strategy for casting from camera and what to do.
+    /// </summary>
+    private delegate void CastStrategy();
 
     /// <summary>
     /// Sets the followed <see cref="GameObject" />
@@ -33,10 +45,37 @@ public class CameraController : MonoBehaviour
         {
             transform.position = player.transform.position - (Distance * direction);
         };
+
+        this.castStrategy = () =>
+        {
+            RaycastHit hit;
+            if (Physics.Linecast(transform.position, player.transform.position, out hit))
+            {
+                if (hit.collider.gameObject.tag == "Wall")
+                {
+                    MeshRenderer meshRenderer = hit.collider.gameObject.GetComponent<MeshRenderer>();
+                    meshRenderer.enabled = false;
+                    disabledRenderers.Add(meshRenderer);
+                }
+            }
+        };
     }
-    
+
     void LateUpdate()
     {
         updateStrategy();
+    }
+
+    void Update()
+    {
+        ReEnableDisabledMeshRenderers();
+        castStrategy();
+    }
+
+    private void ReEnableDisabledMeshRenderers() { 
+        disabledRenderers.ForEach(p => {
+            p.enabled = true;
+        });
+        disabledRenderers.Clear();
     }
 }
